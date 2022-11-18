718576f083d7d57cc5d6542952808d148a0a2f8e
bobbyfolarin11 <103668703+bobbyfolarin11@users.noreply.github.com>
2022-11-19 00:31:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   let i;
-  let slides = document.getElementsByClassName("mySlides");
+  let slides = document.getElementsByClassName("mySlide");
   let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
